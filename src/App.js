a757f1fca6bab24f8a2e8d1b6a7f9df90cf9cb37
Srujan Shah <shahsrujan@gmail.com>
2019-07-26 12:55:24 -0700

    check boxes working using setState

--- src/App.js
+++ src/App.js
@@ -3,12 +3,36 @@ import ToDoItem from "./components/ToDoItem";
 import todosData from "./todosData";
 import "./style.css";
 
-function App() {
-  const todoItems = todosData.map(item => (
-    <ToDoItem key={item.id} item={item} />
-  ));
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-  return <div className="todo-list">{todoItems}</div>;
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+      return {
+        todos: updatedTodos
+      };
+    });
+  }
+
+  render() {
+    const todoItems = this.state.todos.map(item => (
+      <ToDoItem key={item.id} item={item} handleChange={this.handleChange} />
+    ));
+
+    return <div className="todo-list">{todoItems}</div>;
+  }
 }
 
 export default App;
